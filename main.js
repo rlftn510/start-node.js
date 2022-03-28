97f6c5792a8d4bf2bca0303c59c728a01ee2dfe2
walter.yu <walter.yu@kinemaster.com>
2022-03-28 21:15:40 +0900

    코드 정리(백업)

--- main.js
+++ main.js
@@ -1,7 +1,8 @@
 var http = require("http");
 var url = require("url");
 // var sanitizeHtml = require("sanitize-html");
-var topic = require(".//lib/topic");
+var topic = require("./lib/topic");
+var author = require("./lib/author");
 // db.connect();
 
 var app = http.createServer(function (request, response) {
@@ -11,104 +12,29 @@ var app = http.createServer(function (request, response) {
   if (pathname === "/") {
     if (queryData.id === undefined) {
       topic.home(request, response);
-      // fs.readdir("./data", function (error, filelist) {
-      //   var title = "Welcome";
-      //   var description = "Hello, Node.js";
-      //   var list = template.list(filelist);
-      //   var html = template.HTML(
-      //     title,
-      //     list,
-      //     `<h2>${title}</h2>${description}`,
-      //     `<a href="/create">create</a>`
-      //   );
-      //   response.writeHead(200);
-      //   response.end(html);
-      // });
     } else {
       topic.page(request, response);
-      /*fs.readdir("./data", function (error, filelist) {
-        var filteredId = path.parse(queryData.id).base;
-        fs.readFile(`data/${filteredId}`, "utf8", function (err, description) {
-          var title = queryData.id;
-          var sanitizedTitle = sanitizeHtml(title);
-          var sanitizedDescription = sanitizeHtml(description, {
-            allowedTags: ["h1"],
-          });
-          var list = template.list(filelist);
-          var html = template.HTML(
-            sanitizedTitle,
-            list,
-            `<h2>${sanitizedTitle}</h2>${sanitizedDescription}`,
-            ` <a href="/create">create</a>
-                <a href="/update?id=${sanitizedTitle}">update</a>
-                <form action="delete_process" method="post">
-                  <input type="hidden" name="id" value="${sanitizedTitle}">
-                  <input type="submit" value="delete">
-                </form>`
-          );
-          response.writeHead(200);
-          response.end(html);
-        })
-      });;*/
     }
   } else if (pathname === "/create") {
     topic.create(request, response);
-    /*fs.readdir("./data", function (error, filelist) {
-      var title = "WEB - create";
-      var list = template.list(filelist);
-      var html = template.HTML(
-        title,
-        list,
-        `
-          <form action="/create_process" method="post">
-            <p><input type="text" name="title" placeholder="title"></p>
-            <p>
-              <textarea name="description" placeholder="description"></textarea>
-            </p>
-            <p>
-              <input type="submit">
-            </p>
-          </form>
-        `,
-        ""
-      );
-      response.writeHead(200);
-      response.end(html);
-    });*/
   } else if (pathname === "/create_process") {
     topic.create_process(request, response);
   } else if (pathname === "/update") {
     topic.update(request, response);
-    /*fs.readdir("./data", function (error, filelist) {
-      var filteredId = path.parse(queryData.id).base;
-      fs.readFile(`data/${filteredId}`, "utf8", function (err, description) {
-        var title = queryData.id;
-        var list = template.list(filelist);
-        var html = template.HTML(
-          title,
-          list,
-          `
-            <form action="/update_process" method="post">
-              <input type="hidden" name="id" value="${title}">
-              <p><input type="text" name="title" placeholder="title" value="${title}"></p>
-              <p>
-                <textarea name="description" placeholder="description">${description}</textarea>
-              </p>
-              <p>
-                <input type="submit">
-              </p>
-            </form>
-            `,
-          `<a href="/create">create</a> <a href="/update?id=${title}">update</a>`
-        );
-        response.writeHead(200);
-        response.end(html);
-      });
-    });*/
   } else if (pathname === "/update_process") {
     topic.update_process(request, response);
   } else if (pathname === "/delete_process") {
     topic.delete_process(request, response);
+  } else if (pathname === "/author") {
+    author.home(request, response);
+  } else if (pathname === "/author/create_process") {
+    author.create_process(request, response);
+  } else if (pathname === "/author/update") {
+    author.update(request, response);
+  } else if (pathname === "/author/update_process") {
+    author.update_process(request, response);
+  } else if (pathname === "/author/delete_process") {
+    author.delete_process(request, response);
   } else {
     response.writeHead(404);
     response.end("Not found");
